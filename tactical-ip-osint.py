dbbbb8994183eedf75369dbe0c052cdb0cee5362
FBI Tactical <71570618+FBITactical@users.noreply.github.com>
2020-09-20 18:45:15 +0000

    Add files via upload

---
+++ tactical-ip-osint.py
@@ -0,0 +1,23 @@
+#An-IP-Address-Information-Gathering-Tool
+#Coded by:fbitactical on Instagram.
+
+import requests
+import sys
+
+print()
+print("▄▄▄█████▓ ▄▄▄       ▄████▄  ▄▄▄█████▓ ██▓ ▄████▄   ▄▄▄       ██▓ ")   
+print("▓  ██▒ ▓▒▒████▄    ▒██▀ ▀█  ▓  ██▒ ▓▒▓██▒▒██▀ ▀█  ▒████▄    ▓██▒ ")   
+print("▒ ▓██░ ▒░▒██  ▀█▄  ▒▓█    ▄ ▒ ▓██░ ▒░▒██▒▒▓█    ▄ ▒██  ▀█▄  ▒██░ ")   
+print("░ ▓██▓ ░ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒░ ▓██▓ ░ ░██░▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██░   ") 
+print("  ▒██▒ ░  ▓█   ▓██▒▒ ▓███▀ ░  ▒██▒ ░ ░██░▒ ▓███▀ ░ ▓█   ▓██▒░██████▒ ")
+print("  ▒ ░░    ▒▒   ▓▒█░░ ░▒ ▒  ░  ▒ ░░   ░▓  ░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ▒░▓  ░ ")
+print("    ░      ▒   ▒▒ ░  ░  ▒       ░     ▒ ░  ░  ▒     ▒   ▒▒ ░░ ░ ▒  ░ ")
+print("  ░        ░   ▒   ░          ░       ▒ ░░          ░   ▒     ░ ░    ")
+print("               ░  ░░ ░                ░  ░ ░            ░  ░    ░  ░ ")
+print("                   ░                     ░                           ")
+print()
+
+ip_address = input("Enter an ip-address: ")
+response = requests.get("http://ip-api.com/json/" + ip_address)
+
+print(response.json())
